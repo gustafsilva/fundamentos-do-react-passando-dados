bb8f531b2223813b728e87fcf9fc1c9f3a97193a
Gustavo F. Silva <gustafsilva@hotmail.com>
2018-12-06 00:41:27 +0000

    Criando componente para renderizar lista de filmes favoritos.

---
+++ src/ListFavoriteMovies.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ListFavoriteMovies extends Component {
+  renderFavoriteMovie(favoriteMovie) {
+    const { user, movie, profileID } = favoriteMovie;
+    
+    return (
+    	<li key={profileID}>{user.name} favorite movie is {movie.name}.</li>
+    );
+  }
+  
+  render() {   
+   return (
+     <ul>
+     	{ this.props.favoriteMovies.map(favoriteMovie => this.renderFavoriteMovie(favoriteMovie)) }
+     </ul>
+   ); 
+  }  
+}
+
+export default ListFavoriteMovies;
